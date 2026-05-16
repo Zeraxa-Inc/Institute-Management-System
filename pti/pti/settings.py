5ccdc1de76c557b0cfa072491a3c2acc8eed91f2
JRG <janakrajgaha@gmail.com>
2026-05-16 19:24:25 +0545

    IMS Initial0.2

--- pti/pti/settings.py
+++ pti/pti/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ptimain.apps.PtimainConfig',
+    'ptiadmin.apps.PtiadminConfig',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'pti.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mydatabase",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
